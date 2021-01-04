9c87586e8335caa0e83dd931bdb11bf1f528d616
as <>
2021-01-04 12:57:42 +0900

    イガグリを的に飛ばす

--- Assets/igaguriController.cs
+++ Assets/igaguriController.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class igaguriController : MonoBehaviour
+public class Igaguricontroller : MonoBehaviour
 {
+    public void shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shoot(new Vector3(0, 200, 2000));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
